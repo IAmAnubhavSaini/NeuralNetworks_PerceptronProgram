08b0a6d5a9a63bbb0961917fe616c11f91f1fb50
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-21 19:33:15 +0530

    ComputeOutput is working as expected.

--- PerceptronProgram/Perceptron.cs
+++ PerceptronProgram/Perceptron.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 namespace Perceptrons
 {
     public class Perceptron
@@ -10,18 +12,41 @@ namespace Perceptrons
         int Output;
         Random rnd;
 
-        public Perceptron(int numInput) {
+        public Perceptron(int numInput)
+        {
             NumInput = numInput;
             Inputs = new double[NumInput];
             Weights = new double[NumInput];
             rnd = new Random(0);
             InitializeWeights();
         }
-        public int ComputeOutput(double[] xValues) { throw new NotImplementedException(); }
+        public int ComputeOutput(double[] xValues)
+        {
+            if (xValues == null)
+                throw new ArgumentNullException("xValues cannot be null.");
+            if (xValues.Length != NumInput)
+                throw new ArgumentException("Bad xValues data provided.");
+
+            for (int i = 0; i < xValues.Length; i++)
+            {
+                Inputs[i] = xValues[i];
+            }
+            double sum = 0.0;
+            for (int i = 0; i < NumInput; i++)
+            {
+                sum += Inputs[i] * Weights[i];
+            }
+            sum += Bias;
+            int result = Activation(sum);
+            Output = result;
+            return result;
+        }
+
         public double[] Train(double[][] trainingData, double alpha, int maxEpochs) { throw new NotImplementedException(); }
         void Shuffle(int[] sequence) { throw new NotImplementedException(); }
         void Update(int computed, int desired, double alpha) { throw new NotImplementedException(); }
-        void InitializeWeights() {
+        void InitializeWeights()
+        {
             double low = -0.01;
             double high = 0.01;
             for (int i = 0; i < Weights.Length; i++)
@@ -30,6 +55,12 @@ namespace Perceptrons
             }
             Bias = (high - low) * rnd.NextDouble() + low;
         }
-        static int Activation(double v) { throw new NotImplementedException(); }
+        static int Activation(double v)
+        {
+            if (v > 0.0)
+                return +1;
+            else
+                return -1;
+        }
     }
 }
