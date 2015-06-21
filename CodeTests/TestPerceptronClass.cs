f582356de40ddcc4a394835a7cf7c19a79fdee3c
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-21 19:07:43 +0530

    Failing test for Perceptron constructor

---
+++ CodeTests/TestPerceptronClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Perceptrons;
+
+namespace CodeTests
+{
+    [TestClass]
+    public class TestPerceptronClass
+    {
+        [TestMethod]
+        public void PerceptronObjectIsCreatedSuccessfully()
+        {
+            Perceptron perceptron = new Perceptron(4);
+            Assert.IsNotNull(perceptron);
+            Assert.IsInstanceOfType(perceptron, typeof(Perceptron));
+        }
+    }
+}
