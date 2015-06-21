0f7e3d3cf9102a81072fc39d22ba2c8f422a5f2b
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-21 06:21:01 +0530

    Initial State of code.

---
+++ CodeTests/TestUIElements.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Perceptrons;
+
+namespace CodeTests
+{
+    [TestClass]
+    public class TestUIElements
+    {
+        static char givenCharacter = '-';
+        static int giveCharacterCount = 26;
+        static string separator = UIElements.Separator(givenCharacter, giveCharacterCount);
+        
+        [TestMethod]
+        public void TestSeparatorForLength()
+        {
+            Assert.AreEqual(separator.Length, 1 + giveCharacterCount + 1);
+        }
+
+        [TestMethod]
+        public void TestThatSeparatorContainsNewLines()
+        {
+            Assert.AreEqual(separator.Contains("\n"), true);
+        }
+
+        [TestMethod]
+        public void TestThatSeparatorContainsTwoNewLineCharacters()
+        {
+            int countNewLines = 0;
+            for (int i = 0; i < separator.Length; i++)
+            {
+                if (separator[i] == '\n')
+                    countNewLines++;
+            }
+            Assert.AreEqual(countNewLines, 2);
+        }
+
+        [TestMethod]
+        public void TestThatSeparatorContainsGivenCharacterGivenCountTimes()
+        {
+            int countGivenCharacter = 0;
+            for (int i = 0; i < separator.Length; i++)
+            {
+                if (separator[i] == givenCharacter)
+                    countGivenCharacter++;
+            }
+            Assert.AreEqual(countGivenCharacter, giveCharacterCount);
+            
+        }
+    }
+}
---
+++ PerceptronProgram/PerceptronProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Perceptrons
+{
+    class PerceptronProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UIElements.Banner('=', 26, "Begin Perceptron demo."));
+            Console.WriteLine(UIElements.Banner('=', 26, "End Perceptron demo."));
+            Console.ReadLine();
+        }
+    }
+
+    public class UIElements
+    {
+        public static string Banner(char ch, int count, string message)
+        {
+            return Separator(ch, count) + message + Separator(ch, count);
+        }
+
+        public static string Separator(char toPrint, int count)
+        {
+            string separators = "\n";
+            for (int i = 1; i <= count; i++)
+                separators += toPrint;
+            separators += "\n";
+            return separators;
+        }
+    }
+}
