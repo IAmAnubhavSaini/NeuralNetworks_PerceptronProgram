8afe5dd9d817f66f03f2199a28600a2baab23472
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-21 18:55:29 +0530

    Perceptron class added. Empty-ish

---
+++ PerceptronProgram/Perceptron.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Perceptrons
+{
+    public class Perceptron
+    {
+        int numInput;
+        double[] inputs;
+        double[] weights;
+        double bias;
+        int output;
+        Random rnd;
+
+        public Perceptron(int numInput) { throw new NotImplementedException; }
+        public int ComputeOutput(double[] xValues) { throw new NotImplementedException; }
+        public double[] Train(double[][] trainingData, double alpha, int maxEpochs) { throw new NotImplementedException; }
+        void Shuffle(int[] sequence) { throw new NotImplementedException; }
+        void Update(int computed, int desired, double alpha) { throw new NotImplementedException; }
+        void InitializeWeights() { throw new NotImplementedException; }
+        static int Activation(double v) { throw new NotImplementedException; }
+    }
+}
