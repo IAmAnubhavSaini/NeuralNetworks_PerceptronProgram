c00686aba38fcb4c1d8200bf1a0650187aa732db
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-21 19:08:22 +0530

    Initializing constructor properly. Failing test now fails not any more.

--- PerceptronProgram/Perceptron.cs
+++ PerceptronProgram/Perceptron.cs
@@ -3,19 +3,33 @@ namespace Perceptrons
 {
     public class Perceptron
     {
-        int numInput;
-        double[] inputs;
-        double[] weights;
-        double bias;
-        int output;
+        int NumInput;
+        double[] Inputs;
+        double[] Weights;
+        double Bias;
+        int Output;
         Random rnd;
 
-        public Perceptron(int numInput) { throw new NotImplementedException; }
-        public int ComputeOutput(double[] xValues) { throw new NotImplementedException; }
-        public double[] Train(double[][] trainingData, double alpha, int maxEpochs) { throw new NotImplementedException; }
-        void Shuffle(int[] sequence) { throw new NotImplementedException; }
-        void Update(int computed, int desired, double alpha) { throw new NotImplementedException; }
-        void InitializeWeights() { throw new NotImplementedException; }
-        static int Activation(double v) { throw new NotImplementedException; }
+        public Perceptron(int numInput) {
+            NumInput = numInput;
+            Inputs = new double[NumInput];
+            Weights = new double[NumInput];
+            rnd = new Random(0);
+            InitializeWeights();
+        }
+        public int ComputeOutput(double[] xValues) { throw new NotImplementedException(); }
+        public double[] Train(double[][] trainingData, double alpha, int maxEpochs) { throw new NotImplementedException(); }
+        void Shuffle(int[] sequence) { throw new NotImplementedException(); }
+        void Update(int computed, int desired, double alpha) { throw new NotImplementedException(); }
+        void InitializeWeights() {
+            double low = -0.01;
+            double high = 0.01;
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                Weights[i] = (high - low) * rnd.NextDouble() + low;
+            }
+            Bias = (high - low) * rnd.NextDouble() + low;
+        }
+        static int Activation(double v) { throw new NotImplementedException(); }
     }
 }
